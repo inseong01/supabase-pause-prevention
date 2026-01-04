d648217c06d3ed8a46aba6f0aa5bd5bb0e1e8f71
inseong01 <insung940@naver.com>
2026-01-04 11:43:26 +0900

    refactor: table 이름 수정
    - keep_alive -> keep-alive

--- config/keep-alive-config.ts
+++ config/keep-alive-config.ts
@@ -2,7 +2,7 @@ export type KeepAliveConfig = typeof keepAliveConfig;
 
 export const keepAliveConfig = {
   // Select a table in your Supabase database to make a call to
-  table: 'keep_alive',
+  table: 'keep-alive',
 
   // Column that will be queried with a random string
   column: 'name',
